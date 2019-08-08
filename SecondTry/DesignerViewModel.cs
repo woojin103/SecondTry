e366ec70d55b19f9654493d62f88482eb4ccb7d9
우진 조 <chowoojin103@naver.com>
2019-08-08 18:47:11 +0900

    초기 커밋.

---
+++ SecondTry/DesignerViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.Activities.Core.Presentation;
+using System.Activities.Presentation;
+using System.Activities.Presentation.Metadata;
+using System.Activities.Presentation.Toolbox;
+using System.Windows.Input;
+
+namespace SecondTry
+{
+    class DesignerViewModel
+    {
+        public ICommand NewCommand { get; set; }
+        public ICommand OpenCommand { get; set; }
+        public ICommand SaveCommand { get; set; }
+        public ICommand SaveAsCommand { get; set; }
+        public ICommand ExitCommand { get; set; }
+
+        public string FileName
+        {
+            get
+            {
+                return this.FileName;
+            }
+
+            private set
+            {
+                this.fileName = value;
+                this.Title = string.Format("{0} - {1}", title, this.FileName);
+            }
+        }
+
+        public WorkflowDesigner RehostedWFDesigner
+        {
+            get
+            {
+                return this.rehostedWFDesigner;
+            }
+
+            private set
+            {
+                this.rehostedWFDesigner = value;
+                this.NotifyChanged("WorkflowDesignerControl");
+                this.NotifyChanged("WorkflowPropertyControl");
+            }
+        }
+
+        public object WorkflowDesignerControl
+        {
+            get
+            {
+                return this.RehostedWFDesigner.View;
+            }
+        }
+
+        public object WorkflowPropertyControl
+        {
+            get
+            {
+                return this.RehostedWFDesigner.PropertyInspectorView;
+            }
+        }
+
+        public string XAML
+        {
+            get
+            {
+                if (this.RehostedWFDesigner.Text != null)
+                {
+                    this.RehostedWFDesigner.Flush();
+                    return this.RehostedWFDesigner.Text;
+                }
+
+                return null;
+            }
+        }
+    }
+
+
+}
---
+++ SecondTry/RunCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SecondTry
+{
+    class RunCommand
+    {
+        private readonly Predicate<object> canExecute;
+
+        /// <summary>  
+        /// Action to be executed when the command is invoked  
+        /// </summary>  
+        private readonly Action<object> execute;
+
+        /// <summary>  
+        /// Creates a new command  
+        /// </summary>  
+        /// <param name="execute"></param>  
+        /// <param name="canExecute"></param>  
+        public RunCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+    }
+}
